9b8e0a271cf3f20b7656b5fbcc4a723809546270
Aidana <zholdassova.aidana@mail.ru>
2021-12-06 22:47:33 +0600

    second commit

--- src/test/java/lesson4/ForGit.java
+++ src/test/java/lesson4/ForGit.java
@@ -1,7 +1,11 @@
 package lesson4;
 
+import org.junit.jupiter.api.Test;
+
 public class ForGit {
     void testingMethod() {
         // тут должен быть какой то умный коммент
+        @Test
+        // тут какой то супер код
     }
 }
