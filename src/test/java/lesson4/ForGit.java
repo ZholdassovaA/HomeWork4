dbe799515aecad4dc8e5a8d1c7747a8d2baa879d
Aidana <zholdassova.aidana@mail.ru>
2021-12-06 22:34:49 +0600

    combined 3 commits

---
+++ src/test/java/lesson4/ForGit.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+import org.junit.jupiter.api.Test;
+
+public class ForGit {
+    void testingMethod() {
+        // тут должен быть какой то умный коммент
+        @Test
+        // тут какой то супер код
+        @Test
+        // еще один крутой код
+    }
+
+}
